286cccf56423b5bd7f561786d5d162cdca0e38cd
Lawrence Warren <lawrencewarren2@gmail.com>
2023-05-17 13:39:56 +0100

    fix(ts): Add doc comments

--- src/lib/google_helpers/oauth_client.ts
+++ src/lib/google_helpers/oauth_client.ts
@@ -9,6 +9,11 @@ const GOOGLE_OAUTH2_CLIENT = new google.auth.OAuth2({
 
 const SCOPES = ['https://www.googleapis.com/auth/calendar.events'];
 
+/**
+ * Retrieve the URL that allows users to grant permissions to read and write
+ * their Google Calendars.
+ * @returns The Google OAuth URL
+ */
 export function google_get_oauth_req_url() {
 	const url = GOOGLE_OAUTH2_CLIENT.generateAuthUrl({
 		access_type: 'offline', // offline gets refresh token always
@@ -18,20 +23,36 @@ export function google_get_oauth_req_url() {
 	return url;
 }
 
+/**
+ * Identify if OAuth credentials are set.
+ * @returns `true` if OAuth is set, otherwise `false`
+ */
 export function google_get_is_oauth_set() {
 	return GOOGLE_OAUTH2_CLIENT.credentials;
 }
 
+/**
+ * Set the Google OAuth credentials.
+ * @param code The OAuth code returned once a user has granted permissions. Used
+ * to generate OAuth tokens
+ */
 export async function google_set_oauth2_credentials(code: string) {
 	const { tokens } = await GOOGLE_OAUTH2_CLIENT.getToken(code);
 	GOOGLE_OAUTH2_CLIENT.setCredentials(tokens);
 	// TODO: Save tokens.access_token & tokens.refresh_token to a database
 }
 
+/**
+ * Clears any set OAuth credentials.
+ */
 export async function google_remove_oauth2_credentials() {
 	GOOGLE_OAUTH2_CLIENT.revokeCredentials();
 }
 
+/**
+ * Retrieve a reference to the Google Calendar API object.
+ * @returns A reference to the Google Calendar API object.
+ */
 export function google_get_calendar_api() {
 	return google.calendar({
 		version: 'v3',
--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -2,7 +2,7 @@ import { google_get_oauth_req_url } from '$lib/google_helpers/oauth_client';
 
 import type { PageServerLoad } from './$types';
 
-export const load = (({}) => {
+export const load = (() => {
 	return {
 		google_auth_url: google_get_oauth_req_url()
 	};
