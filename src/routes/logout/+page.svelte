bef5000cc7d470bfcbabb094e6c1fe34304758ce
Lawrence Warren <lawrencewarren2@gmail.com>
2023-07-17 21:39:00 +0100

    feat(ts): Update Login route, create Logout route

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -6,18 +6,17 @@
 
   import type { PageData } from './$types';
 	export let data: PageData;
-
-  async function login(auth0Client: Auth0Client) {
-    await auth.login_with_popup(auth0Client);
+  
+  async function logout(auth0Client: Auth0Client) {
+    await auth.logout(auth0Client);
   }
 
   onMount(async () => {
     const auth0Client = await auth.create_client(data.auth0_domain, data.auth0_client_id);
-    console.log("Logging in...")
-    await login(auth0Client);
+    console.log("Logging out...")
+    await logout(auth0Client);
     console.log("Popup closed")
     isAuthenticated.set(await auth0Client.isAuthenticated());
-    const USER = await auth0Client.getUser() as object
-    user.set(USER);
+    user.set(await auth0Client.getUser() as object);
   });
 </script>
