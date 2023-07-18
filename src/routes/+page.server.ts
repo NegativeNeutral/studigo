184b1365f6e4331c507d3e0d55a4dbd0a7d93fef
Lawrence Warren <lawrencewarren2@gmail.com>
2023-07-18 20:03:51 +0100

    minor(ts): Remove redundant params

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,7 @@
 import { vercel_get_all_studios } from '$lib/helpers/vercel/postgres_client';
-
 import type { PageServerLoad } from './$types';
 
-export const load = (async ({ url, params }) => {
+export const load = (async () => {
 	const O = vercel_get_all_studios();
 	return { all_studios: O };
 }) satisfies PageServerLoad;
